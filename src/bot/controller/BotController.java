d675d7f7c94bfad012f6b62e69dc8db1e4c6c5f2
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-10 19:36:14 -0700

    Adding starting code, and added a file reader for config.

--- src/bot/controller/BotController.java
+++ src/bot/controller/BotController.java
@@ -1,13 +1,43 @@
 package bot.controller;
 
+import sx.blah.discord.api.ClientBuilder;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.util.DiscordException;
+
 public class BotController 
 {
+	public static final IDiscordClient bot = createClient(FileReader.readConfig(), true);
+	
 	public BotController()
 	{
 		
 	}
 	void start()
 	{
-		
+		System.out.println(bot.getApplicationClientID());
 	}
+	
+	
+	
+	public static IDiscordClient createClient(String token, boolean logged)
+    {
+        ClientBuilder buildClient = new ClientBuilder();
+        buildClient.withToken(token);
+        try
+        {
+            if (logged)
+            {
+                return buildClient.login();
+            }
+            else
+            {
+                return buildClient.build();
+            }
+        }
+        catch (DiscordException event)
+        {
+            event.printStackTrace();
+            return null;
+        }
+    }
 }
---
+++ src/bot/controller/FileReader.java
@@ -0,0 +1,25 @@
+package bot.controller;
+import java.io.*;
+import java.util.Scanner;
+
+public class FileReader 
+{
+	public static String readConfig()
+	{
+		String container = "";
+		String file = "Config.txt";
+		try
+		{
+			Scanner scanFile = new Scanner(new File(file));
+				String row = scanFile.next();
+				container = row;
+				scanFile.close();
+		}
+		
+		catch (FileNotFoundException error)
+		{
+			System.out.println("There was an error:" + error.getMessage());
+		}
+			return container;
+	}
+}
