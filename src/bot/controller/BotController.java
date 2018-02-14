09293ef0da6cb4c9a42617708a065e05c2c4dc00
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-14 13:19:41 -0700

    Making the code my own and adding comments to understand my code better.

--- src/bot/controller/BotController.java
+++ src/bot/controller/BotController.java
@@ -50,52 +50,15 @@ public class BotController
         }
     }
 	
-	public static void processMessage(IMessage message, String prefix)
+	public static void processCommand(IMessage message, String prefix)
 	{
 		IUser member = message.getAuthor();
 		IChannel textChan = message.getChannel();
 		IGuild server = message.getGuild();
 		
-		String[] respondTo = message.getContent().toLowerCase().replaceFirst(prefix, "").split(" ");
 		
-		if (respondTo[0].equals("ping"))
-		{
-			textChan.sendMessage("pong!");
-		}
 		
-		else if(respondTo[0].equals("avatar"))
-		{
-			if (respondTo.length >= 3)
-			{
-				respondTo[1] = respondTo[1].replaceAll("[<>@!]", "");
-				
-				for(char letters = 'A'; letters <= 'Z'; letters++)
-				{
-                    if(respondTo[1].toUpperCase().indexOf(letters) >= 0)
-                    {
-                       
-                        textChan.sendMessage(respondTo[1] + " Is not a valid user!");
-                        return;
-                       
-                    }
-                   
-                }
-				
-				IUser actor = server.getUserByID(Long.parseLong(respondTo[1]));
-				
-				textChan.sendMessage(actor.mention() + "'s picture: " + actor.getAvatarURL());
-				message.delete();
-				return;
-			}
-			
-			else
-			{
-				textChan.sendMessage("Not a good argument.");
-			}
-		}
 	}
-	
-	
 	
 	
 }
--- src/bot/controller/BotListener.java
+++ src/bot/controller/BotListener.java
@@ -3,16 +3,20 @@ package bot.controller;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 
+// This listens to messages with a specific prefix.
 public class BotListener 
 {
+// This declares and initializes a String called prefix with of course, a prefix as String.
 public static String prefix = new String("/");
 	
+	// This gets the message sent by a user and checks if it starts with the correct prefix. If so we call processCommand
+	// to take care of the message and understand what to do with it.
 	@EventSubscriber
 	public void messageEvent(MessageReceivedEvent event)
 	{
 		if (event.getMessage().getContent().toLowerCase().startsWith(prefix))
 		{
-			BotController.processMessage(event.getMessage(), prefix);
+			BotController.processCommand(event.getMessage(), prefix);
 		}
 	}
 }
