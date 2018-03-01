47f04920605f4c7f0fafda19a6eca2b2a24918cb
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-03-01 13:42:11 -0700

    Added a new library, and a new class for a new command.

---
+++ src/bot/model/CatImage.java
@@ -0,0 +1,20 @@
+package bot.model;
+
+import com.google.api.services.customsearch.*; 	
+
+public class CatImage 
+{
+	public CatImage()
+	{
+		
+	}
+	
+	public String grabImage()
+	{
+		String imageURL = "";
+		
+		
+		return imageURL;
+	}
+	
+}
