f2c53e3f9d8912e196edd2271f232ec50da4af62
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-03-08 10:19:51 -0700

    Trying to do a different command instead for a game, but it isn't going so well.

---
+++ src/bot/model/RockPaperScissors.java
@@ -0,0 +1,18 @@
+package bot.model;
+
+public class RockPaperScissors 
+{
+	public RockPaperScissors()
+	{
+		
+	}
+	
+	public String playGame()
+	{
+		String win = " ";
+		
+		if (.processCommand() == "Rock")
+		
+		return win;
+	}
+}
