539bda995298cd2cd5fa28fe1374e0847d7421e9
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-03-13 13:48:48 -0600

    Trying to fix my program, but I'm not suceeding. I added in the code for the game so that it will at least do something, but unfortunally it can't get by the conditionals which means my logic is wrong or something like that.

--- src/bot/model/RockPaperScissors.java
+++ src/bot/model/RockPaperScissors.java
@@ -14,26 +14,79 @@ public class RockPaperScissors implements ICommand {
 	public void execute(MessageReceivedEvent event, String[] args)
 			throws MissingPermissionsException, MissingArgumentsException 
 	{
-		String tempC = Arrays.toString(args);
+		String temp = Arrays.toString(args);
+		temp.replaceFirst("//", "");
 		
-		if (tempC.length() >= 3 && tempC.contains("rps"))
-		{
-			if (tempC.length() == 3)
+			if (temp.length() == 3)
 			{
+				String [] choice = new String[] {"Rock", "Paper", "Scissors"};
+				boolean finished = false;
+				int random = 0;
+				
+				while(finished)
+				{
+					random = (int) (Math.random() * 2);
+					event.getChannel().sendMessage("Enter either, rock, paper, or scissors.");
+					String message = event.getMessage().getContent().toLowerCase();
+					
+					if (message == "rock" || message == "paper" || message == "scissors")
+					{
+						if (message == choice[random])
+						{
+							event.getChannel().sendMessage("Try again. You both tied.");
+						}
+						
+						else if (message == "rock" && choice[random] == "paper")
+						{
+							event.getChannel().sendMessage("The bot wins using paper.");
+							finished = true;
+						}
+						
+						else if (message == "paper" && choice[random] == "rock")
+						{
+							event.getChannel().sendMessage("You won against the bot's rock.");
+							finished = true;
+						}
+						
+						else if (message == "rock" && choice[random] == "scissors")
+						{
+							event.getChannel().sendMessage("You won against the bot's scissors.");
+							finished = true;
+						}
+						
+						else if (message == "scissors" && choice[random] == "rock")
+						{
+							event.getChannel().sendMessage("You lost against the bot's rock.");
+							finished = true;
+						}
+						
+						else if (message == "scissors" && choice[random] == "paper")
+						{
+							event.getChannel().sendMessage("You won against the bot's paper.");
+							finished = true;
+						}
+						
+						else if (message == "paper" && choice[random] == "scissors")
+						{
+							event.getChannel().sendMessage("You lost against the bot's scissors");
+							finished = true;
+						}
+					}
+				}
 				
 			}
 			
-			else if (tempC.length() > 3)
+//			else if (tempC.length() > 3)
+//			{
+//				
+//			}
+			
+			else
 			{
-				
+			//event.getChannel().sendMessage("You did not enter arguments correctly. Either just say rps or rps <@user>.");
+				throw new MissingArgumentsException(getName(), "You did not enter arguments correctly. "
+						+ "Either just say rps or rps <@user>.");
 			}
-		}
-		else
-		{
-			//System.out.println("You did not enter arguments correctly. Either just say rps or rps <@user>.");
-			throw new MissingArgumentsException(getName(), "You did not enter arguments correctly. "
-					+ "Either just say rps or rps <@user>.");
-		}
 	}
 		
 
