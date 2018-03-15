b6bb3918ac691dc95acacfddc7c354917f3de514
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-03-15 14:10:36 -0600

    Trying to fix, but nothing is going according to plan. Everything seems to be breaking.

--- src/bot/model/RockPaperScissors.java
+++ src/bot/model/RockPaperScissors.java
@@ -2,6 +2,7 @@ package bot.model;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
 
 import com.Cardinal.CommandPackage.Commands.ICommand;
 import com.Cardinal.CommandPackage.Exceptions.MissingArgumentsException;
@@ -27,86 +28,113 @@ public class RockPaperScissors implements ICommand {
 				String [] choice = new String[] {"Rock", "Paper", "Scissors"};
 				boolean finished = false;
 				int random = 0;
-				event.getChannel().sendMessage("Enter either, rock, paper, or scissors.");
+				
+				try        
+				{
+					event.getChannel().sendMessage("Enter either, rock, paper, or scissors.");
+				    Thread.sleep(5000);
+				} 
+				catch(InterruptedException interrupt) 
+				{
+				    Thread.currentThread().interrupt();
+				}
 				
 				while(finished == false)
 				{
 					random = (int) (Math.random() * 2);
 					//messageBulk = event.getChannel().getMessageHistory(50);
-					
-					MessageHistory bulk = event.getChannel().getMessageHistory(50);
+					String [] action = new String [1];
+					MessageHistory bulk = event.getChannel().getMessageHistory(10);
 					Iterator<IMessage> move = bulk.iterator();
+					
+					
 					while (move.hasNext())
 					{
 						IMessage msg = (IMessage)move.next();
-						String [] action = new String [3];
 						
-						if (msg.getAuthor() != user && msg.getContent().toLowerCase() != "rock" ||
-								msg.getContent().toLowerCase() != "paper" || msg.getContent().toLowerCase() != "scissors")
+						if (msg.getAuthor() == user)
 						{
-							move.remove();
+							if (msg.getContent().toLowerCase() == "rock" || msg.getContent().toLowerCase() == "paper" ||
+								msg.getContent().toLowerCase() == "scissors")
+							{
+								
+								if (action[0].isEmpty())
+								{
+									action[0] = (msg.toString().toLowerCase());
+								}
+							}
 						}
 					}
 					
-					if (args[0] == "rock" || args[0] == "paper" || args[0] == "scissors")
+					if (action[0].isEmpty())
 					{
-						if (args[0] == choice[random])
+						System.out.println("Could not find your answer. Command reseted.");
+						finished = true;
+					}
+					
+					else
+					{
+						if (action[0] == "rock" || action[0] == "paper" || action[0] == "scissors")
 						{
-							event.getChannel().sendMessage("Try again. You both tied.");
-						}
-						
-						else if (args[0] == "rock" && choice[random] == "paper")
-						{
-							event.getChannel().sendMessage("The bot wins using paper.");
-							finished = true;
-						}
-						
-						else if (args[0] == "paper" && choice[random] == "rock")
-						{
-							event.getChannel().sendMessage("You won against the bot's rock.");
-							finished = true;
-						}
-						
-						else if (args[0] == "rock" && choice[random] == "scissors")
-						{
-							event.getChannel().sendMessage("You won against the bot's scissors.");
-							finished = true;
-						}
-						
-						else if (args[0] == "scissors" && choice[random] == "rock")
-						{
-							event.getChannel().sendMessage("You lost against the bot's rock.");
-							finished = true;
-						}
-						
-						else if (args[0] == "scissors" && choice[random] == "paper")
-						{
-							event.getChannel().sendMessage("You won against the bot's paper.");
-							finished = true;
-						}
-						
-						else if (args[0] == "paper" && choice[random] == "scissors")
-						{
-							event.getChannel().sendMessage("You lost against the bot's scissors");
-							finished = true;
+							if (action[0] == choice[random])
+							{
+								event.getChannel().sendMessage("Try again. You both tied.");
+							}
+							
+							else if (action[0] == "rock" && choice[random] == "paper")
+							{
+								event.getChannel().sendMessage("The bot wins using paper.");
+								finished = true;
+							}
+							
+							else if (action[0] == "paper" && choice[random] == "rock")
+							{
+								event.getChannel().sendMessage("You won against the bot's rock.");
+								finished = true;
+							}
+							
+							else if (action[0] == "rock" && choice[random] == "scissors")
+							{
+								event.getChannel().sendMessage("You won against the bot's scissors.");
+								finished = true;
+							}
+							
+							else if (action[0] == "scissors" && choice[random] == "rock")
+							{
+								event.getChannel().sendMessage("You lost against the bot's rock.");
+								finished = true;
+							}
+							
+							else if (action[0] == "scissors" && choice[random] == "paper")
+							{
+								event.getChannel().sendMessage("You won against the bot's paper.");
+								finished = true;
+							}
+							
+							else if (action[0] == "paper" && choice[random] == "scissors")
+							{
+								event.getChannel().sendMessage("You lost against the bot's scissors");
+								finished = true;
+							}
 						}
 					}
+					
 				}
-				
 			}
+			
+			else
+			{
+				//event.getChannel().sendMessage("You did not enter arguments correctly. Either just say rps or rps <@user>.");
+				throw new MissingArgumentsException(getName(), "You did not enter arguments correctly. "
+						+ "Either just say rps or rps <@user>.");
+			}
+				
+		}
 			
 //			else if (tempC.length() > 3)
 //			{
 //				
 //			}
-			
-			else
-			{
-			//event.getChannel().sendMessage("You did not enter arguments correctly. Either just say rps or rps <@user>.");
-				throw new MissingArgumentsException(getName(), "You did not enter arguments correctly. "
-						+ "Either just say rps or rps <@user>.");
-			}
-	}
 		
 
 	@Override
