98dc883deedfe91fd222e2642cb3f39dae087150
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-03-20 19:08:26 -0600

    Added a new file for reactions an also trying to edit my filereader.

--- src/bot/controller/FileReader.java
+++ src/bot/controller/FileReader.java
@@ -1,5 +1,6 @@
 package bot.controller;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileReader 
@@ -7,34 +8,51 @@ public class FileReader
 	public static String readConfig(String key)
 	{
 		String container = "";
+		ArrayList<String> fileArray = new ArrayList<String>();
 		String file = "src/Config.txt";
+		
 		try
 		{
 			Scanner scanFile = new Scanner(new File(file));
+
+			while (scanFile.hasNext())
+			{
+				String row = scanFile.nextLine();
+				
+				fileArray.add(row);
+			}
 			
-			//String row = scanFile.nextLine();
-				
-				if (scanFile.hasNext(key))
-				{
-					String row = scanFile.nextLine();
-					row = scanFile.nextLine();
-					container = row;
-					
-					scanFile.close();
-				}
-				
-				else
-				{
-					scanFile.close();
-				}
-				//row.replaceFirst(key, " ");	
+			scanFile.close();
 		}
-				
+		
 		catch (FileNotFoundException error)
 		{
 			System.out.println("There was an error:" + error.getMessage());
 		}
 		
-			return container;
+		
+		for (int i = 0; i < fileArray.size(); i++)
+		{
+			if (fileArray.get(i) == key)
+			{
+				container = fileArray.get(i++);
+			}
+		}
+		
+		return container;
+			//String row = scanFile.nextLine();
+				
+//				if (scanFile.hasNext())
+//				{
+//					for (String current : fileArray)
+//					{
+//						
+//					}
+//					String row = scanFile.nextLine();
+//					row = scanFile.nextLine();
+//					container = row;
+//					
+				
+				//row.replaceFirst(key, " ");	
 	}
 }
